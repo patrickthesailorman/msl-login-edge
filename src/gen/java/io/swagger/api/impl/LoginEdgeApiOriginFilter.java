e56b048a666aa7b7345c7d9007c52b63dc5eb501
anram88 <aramirez@kenzanmedia.com>
2016-11-11 02:33:13 -0700

    Improved Archaius configuration

--- src/gen/java/io/swagger/api/impl/LoginEdgeApiOriginFilter.java
+++ src/gen/java/io/swagger/api/impl/LoginEdgeApiOriginFilter.java
@@ -1,6 +1,8 @@
 package io.swagger.api.impl;
 
 import com.google.inject.Inject;
+import com.google.inject.name.Named;
+import com.kenzan.msl.common.utils.Utils;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
@@ -10,13 +12,17 @@ import java.io.IOException;
 public class LoginEdgeApiOriginFilter implements javax.servlet.Filter {
 
     @Inject
+    @Named("clientPort")
+    private static String CLIENT_PORT;
+
+    @Inject
     private static LoginEdgeSessionToken loginEdgeSessionToken;
 
     public void doFilter(ServletRequest request, ServletResponse response,
                          FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
-        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.addHeader("Access-Control-Allow-Origin", Utils.getHost(req.getRequestURL().toString(), CLIENT_PORT));
         res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
         res.addHeader("Access-Control-Allow-Headers", "Content-Type");
         res.addHeader("Access-Control-Allow-Credentials", "true");
@@ -27,4 +33,6 @@ public class LoginEdgeApiOriginFilter implements javax.servlet.Filter {
     public void destroy() {}
 
     public void init(FilterConfig filterConfig) throws ServletException {}
+
+
 }
--- src/main/java/com/kenzan/msl/login/edge/config/LoginEdgeModule.java
+++ src/main/java/com/kenzan/msl/login/edge/config/LoginEdgeModule.java
@@ -1,8 +1,12 @@
 package com.kenzan.msl.login.edge.config;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 import com.kenzan.msl.login.edge.services.LoginEdgeService;
 import com.kenzan.msl.login.edge.services.LoginEdgeServiceImpl;
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringProperty;
 import com.netflix.governator.guice.lazy.LazySingletonScope;
 import io.swagger.api.LoginEdgeApiService;
 import io.swagger.api.factories.LoginEdgeApiServiceFactory;
@@ -10,18 +14,44 @@ import io.swagger.api.impl.LoginEdgeApiOriginFilter;
 import io.swagger.api.impl.LoginEdgeApiServiceImpl;
 import io.swagger.api.impl.LoginEdgeSessionToken;
 import io.swagger.api.impl.LoginEdgeSessionTokenImpl;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.File;
+import java.util.Properties;
 
 /**
  * @author Kenzan
  */
 public class LoginEdgeModule  extends AbstractModule {
+
+    private String DEFAULT_CLIENT_PORT = "3000";
+
+    private DynamicStringProperty CLIENT_PORT =
+            DynamicPropertyFactory.getInstance().getStringProperty("clientPort", DEFAULT_CLIENT_PORT);
+
     @Override
     protected void configure() {
+        configureArchaius();
+        bindConstant().annotatedWith(Names.named("clientPort")).to(CLIENT_PORT.get());
+
         requestStaticInjection(LoginEdgeApiServiceFactory.class);
         requestStaticInjection(LoginEdgeApiOriginFilter.class);
+
         bind(LoginEdgeSessionToken.class).to(LoginEdgeSessionTokenImpl.class).in(
                 LazySingletonScope.get());
         bind(LoginEdgeService.class).to(LoginEdgeServiceImpl.class).in(LazySingletonScope.get());
         bind(LoginEdgeApiService.class).to(LoginEdgeApiServiceImpl.class).in(LazySingletonScope.get());
+    }
+
+    private void configureArchaius() {
+        Properties props = System.getProperties();
+        String ENV = props.getProperty("env");
+        if (StringUtils.isEmpty(ENV) || ENV.toLowerCase().contains("local")) {
+            String configUrl = "file://" + System.getProperty("user.dir") + "/../msl-login-edge-config/edge-config.properties";
+            File f = new File(configUrl);
+            if(f.exists() && !f.isDirectory()) {
+                System.setProperty("archaius.configurationSource.additionalUrls", configUrl);
+            }
+        }
     }
 }
